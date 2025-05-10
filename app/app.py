2d43e18225714b64976148af8c4b96a76a2732cd
klitzkeka <klitzkeka@uwplatt.edu>
2025-05-10 12:10:23 -0500

    Added procfile

--- app/app.py
+++ app/app.py
@@ -1,10 +1,9 @@
 # app.py
 from config import Config
-from extensions import app, socketio
+from extensions import app
 from routes import api
 from flask import Flask
 from flask_socketio import SocketIO
-import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +12,3 @@ app.register_blueprint(api, url_prefix='/api')
 
 # Initialize SocketIO
 socketio = SocketIO(app, cors_allowed_origins="*")  # Adjust CORS as needed
-
-if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
