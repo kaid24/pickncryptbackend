0b1717010d733261f52d2dd2f9a9997dec1b1609
klitzkeka <klitzkeka@uwplatt.edu>
2025-05-10 12:05:39 -0500

    Removed unsafe server practices

--- app/app.py
+++ app/app.py
@@ -2,9 +2,17 @@
 from config import Config
 from extensions import app, socketio
 from routes import api
+from flask import Flask
+from flask_socketio import SocketIO
+import os
 
+app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(api, url_prefix='/api')
 
-if __name__ == '__main__':
-    socketio.run(app, port=8080)
+
+# Initialize SocketIO
+socketio = SocketIO(app, cors_allowed_origins="*")  # Adjust CORS as needed
+
+if __name__ == "__main__":
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
